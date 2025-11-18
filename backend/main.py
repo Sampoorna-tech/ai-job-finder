a613235ce6c243714abbd5e8fe6c69d79011a045
Sampoorna-tech <bhavik.bhadra@gmail.com>
2025-11-18 11:50:40 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,13 +8,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="India IT Job Finder")
 
-origins = [
-    "http://localhost:5173",
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],      # allow all sites (simple for now)
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
